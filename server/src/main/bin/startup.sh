1e2186792d87adb5ddec2d7a751f0c1bf8a4bb1d
NiuNannan <niu.nannan@ports1961.com>
2024-08-12 15:37:24 +0800

    删除知识星球相关链接

--- server/src/main/bin/startup.sh
+++ server/src/main/bin/startup.sh
@@ -53,9 +53,6 @@ else
   echo "Starting kkFileView..."
   nohup java -Dfile.encoding=UTF-8 -Dspring.config.location=../config/application.properties -jar kkFileView-4.4.0-beta.jar > ../log/kkFileView.log 2>&1 &
   echo "Please execute ./showlog.sh to check log for more information"
-  echo "You can get help in our official home site: https://kkview.cn"
-  echo "If you need further help, please join our kk opensource community: https://t.zsxq.com/09ZHSXbsQ"
-  echo "If this project is helpful to you, please star it on https://gitee.com/kekingcn/file-online-preview/stargazers"
   PROCESS=$(ps -ef | grep -v grep | grep java | grep kkFileView | awk 'NR==1{print $2}')
   # 启动成功后将进程号写入pid文件
   echo "$PROCESS" > "$PID_FILE"
